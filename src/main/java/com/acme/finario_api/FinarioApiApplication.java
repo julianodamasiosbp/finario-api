b819ed15ade98bb3ed9c19a50cb396f9bbd78978
Juliano Damasio <julianodamasiosbp@gmail.com>
2025-05-18 19:24:30 -0300

    my first commit

---
+++ src/main/java/com/acme/finario_api/FinarioApiApplication.java
@@ -0,0 +1,13 @@
+package com.acme.finario_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinarioApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinarioApiApplication.class, args);
+	}
+
+}
